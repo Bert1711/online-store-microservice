12d83e4265b4a4a74d30252709603e1f27e473ff
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-03-05 15:01:02 +0300

    "Создан интерфейс OrderRepository"

--- order-service/src/main/java/com/zrn/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/zrn/orderservice/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.zrn.orderservice.repository;
 
+import com.zrn.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author Zaroyan
  */
-public interface OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
