5b80ddae00c39f36254194f00afc114312814c8f
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-03-05 14:11:47 +0300

    "Создан класс Order"

---
+++ order-service/src/main/java/com/zrn/orderservice/model/Order.java
@@ -0,0 +1,27 @@
+package com.zrn.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author Zaroyan
+ */
+@Entity
+@Table(name="t_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
