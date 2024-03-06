83179f3ca29be327cfc1a36ed214bffb34fbe67e
Albert Zaroyan <albertzaroyan@yandex.ru>
2024-03-06 14:35:51 +0300

    "Создан ProductService"

--- product-service/src/main/java/com/zrn/productservice/service/ProductService.java
+++ product-service/src/main/java/com/zrn/productservice/service/ProductService.java
@@ -1,7 +1,42 @@
 package com.zrn.productservice.service;
 
-/**
- * @author Zaroyan
- */
+import com.zrn.productservice.dto.ProductRequest;
+import com.zrn.productservice.dto.ProductResponse;
+import com.zrn.productservice.model.Product;
+import com.zrn.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Продукт {} добавлен", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 }
